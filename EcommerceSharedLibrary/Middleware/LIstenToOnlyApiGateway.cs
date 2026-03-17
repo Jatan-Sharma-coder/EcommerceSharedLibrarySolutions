5f6b1800608e68bd0a67f3435c50708586faa6b1
Jatan-Sharma-Code <JatanSharma475@gmail.com>
2026-03-17 18:45:16 +0530

    Committing DEV-1 API Gateway Middleware is created

---
+++ EcommerceSharedLibrary/Middleware/LIstenToOnlyApiGateway.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceSharedLibrary.Middleware
+{
+    public class LIstenToOnlyApiGateway(RequestDelegate next)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry, service is unavailable");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
