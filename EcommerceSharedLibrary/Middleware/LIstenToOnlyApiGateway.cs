1763c8276f79376c264f2c053d3668913f724312
Jatan-Sharma-Code <JatanSharma475@gmail.com>
2026-03-17 18:46:27 +0530

    Committing DEV-1 API Gateway Middleware is created

--- EcommerceSharedLibrary/Middleware/LIstenToOnlyApiGateway.cs
+++ EcommerceSharedLibrary/Middleware/LIstenToOnlyApiGateway.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace EcommerceSharedLibrary.Middleware
 {
     public class LIstenToOnlyApiGateway(RequestDelegate next)
